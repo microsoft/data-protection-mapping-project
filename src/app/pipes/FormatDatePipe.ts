697a250db866d910e231caf81d256ce39f8e283d
Matt Kincaid <matt.kincaid@slalom.com>
2020-12-02 03:46:00 -0800

    date formatting with moment

--- src/app/pipes/FormatDatePipe.ts
+++ src/app/pipes/FormatDatePipe.ts
@@ -1,4 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import * as moment from 'moment';
 
 
 @Pipe({ name: 'formatDate' })
@@ -6,7 +7,25 @@ export class formatDatePipe implements PipeTransform {
     constructor() {
     }
 
-    transform(date: string): string {
-      return new Date(date).toLocaleDateString();
+    transform(value: string): string {
+      var date = new Date(value);
+      var format = "MMM D, YYYY";
+
+      /* this code removes the year from showing for a date if it is the same as the current year */    
+      // if (date.getFullYear() == new Date().getFullYear()) {
+      //     format = "MMM D";
+      // }
+
+      // set this true to show the time also
+      //if (includeTime)
+      //    format += ", h:mm A";
+
+      var dt = moment(date);
+
+      // set this true to ignore local time zone.
+      //if (showUtc)
+      //  dt = dt.utc();
+
+      return dt.format(format);
     }
 }
